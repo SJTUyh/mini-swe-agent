906de25639c071b11a4e0952f39ba40753cc8414
SJTUyh <1037452625@qq.com>
2025-11-10 10:20:43 +0800

    change name

--- src/minisweagent/run/extra/swebench.py
+++ src/minisweagent/run/extra/swebench.py
@@ -37,10 +37,10 @@ More information about the usage: [bold green]https://mini-swe-agent.com/latest/
 app = typer.Typer(rich_markup_mode="rich", add_completion=False)
 
 DATASET_MAPPING = {
-    "full": "/home/y30044005/datasets/SWE-Bench",
-    "verified": "/home/y30044005/datasets/SWE-Bench_Verified",
-    "lite": "/home/y30044005/datasets/SWE-Bench_Lite",
-    "multimodal": "/home/y30044005/datasets/SWE-Bench_Multimodal",
+    "full": "/home/y30044005/datasets/SWE-Bench/data",
+    "verified": "/home/y30044005/datasets/SWE-Bench_Verified/data",
+    "lite": "/home/y30044005/datasets/SWE-Bench_Lite/data",
+    "multimodal": "/home/y30044005/datasets/SWE-Bench_Multimodal/data",
     "multilingual": "swe-bench/SWE-Bench_Multilingual",
     "smith": "SWE-bench/SWE-smith",
     "_test": "klieret/swe-bench-dummy-test-dataset",

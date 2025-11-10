288d841631521b200d264e36c47e61886268ab7a
SJTUyh <1037452625@qq.com>
2025-11-10 17:10:16 +0800

    change name

--- src/minisweagent/models/openrouter_model.py
+++ src/minisweagent/models/openrouter_model.py
@@ -73,7 +73,7 @@ class OpenRouterModel:
 
         payload = {
             # "model": self.config.model_name,
-            "model": "/data/weight/QwenCode-2.5-7B/",
+            "model": "/data/weight/DeepSeek-R1-Distill-Qwen-32B/",
             "messages": messages,
             "usage": {"include": True},
             **(self.config.model_kwargs | kwargs),
